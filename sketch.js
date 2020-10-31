cc6730154d7923211fb74ef9a36e9b561f3df20c
Aniket1234-wq <71884021+Aniket1234-wq@users.noreply.github.com>
2020-10-31 17:25:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body; 
+
+var paper;
+var ground;
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	
+	
+ 
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+
+  
+  
+  drawSprites();
+ 
+}
+
+
+
