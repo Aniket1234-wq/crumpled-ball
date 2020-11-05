ca7b518118c2d1d04a0636b21543b957d0dddc80
Aniket1234-wq <71884021+Aniket1234-wq@users.noreply.github.com>
2020-11-05 14:25:08 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -1,22 +1,35 @@
-class Paper {
-    constructor (x, y, radius) {
-        var paper_options = {
-            isStatic : false,
-            restitution : 0.3,
-            friction : 0.5,
-            density : 1.2
-        }
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.body = Bodies.circle(this.x, this.y, this.radius/2, paper_options);
-        this.radius = radius;
-        World.add(world, this.body);
-      }
-      display(){
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rectMode(CENTER);
-        pop();
-      }
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
 }
--- sketch.js
+++ sketch.js
@@ -2,54 +2,64 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body; 
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-var paper;
-var ground;
-
-function preload()
-{
-	
-}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
 
 	engine = Engine.create();
 	world = engine.world;
-
-	//Create the Bodies Here.
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
 	
-	ground = new Ground(200,390,200,20,ground_options);
-    World.add(world,ground);
 
-    ball = new Paper(200, 100, 20,paper_options);
-    World.add(world, ball);
-
- 
-
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
+	//Render.run(render);
   
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background("white");
-
-  Engine.update(engine);
-    rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
-
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x, ball.position.y, 20, 20);
-
+  background(0);
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
   
   
-  drawSprites();
  
 }
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
+
+
 
 
 
